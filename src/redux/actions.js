40249e617736439feab5d59d428a8f6a65462682
Leonardo <leomleao@gmail.com>
2019-02-23 21:24:09 -0300

    Messing with REDUX 🔥

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,3 +1,5 @@
 export * from './menu/actions';
 export * from './settings/actions';
 export * from './auth/actions';
+export * from './todo/actions';
+export * from './project/actions';
--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,8 +1,7 @@
 import { all } from 'redux-saga/effects';
 import authSagas from './auth/saga';
+import todoSagas from './todo/saga';
 
 export default function* rootSaga(getState) {
-  yield all([
-    authSagas()
-  ]);
+	yield all([authSagas(), todoSagas()]);
 }
